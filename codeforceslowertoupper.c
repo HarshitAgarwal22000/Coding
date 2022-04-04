e2660f1fbd9c19bc707c21beb743f8e49b607b29
HarshitAgarwal22000 <harshitiit2003@gmail.com>
2022-04-05 00:38:20 +0530

    commit

--- codeforceslowertoupper.c
+++ codeforceslowertoupper.c
@@ -1,1 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    char c[1000]={};
+    int count,count_1;
+    scanf("%s",c);
+    for(int i=0;i<1000;i++)
+    {
+        if(islower(c[i]))
+        {
+            count_1=count_1+1;
+        }
+        else if(isupper(c[i]))
+        {
+            count=count+1;
+        }
+    }
+    for(int j=0;j<1000;j++)
+    {
+        
+        if(count_1>count)
+        {
+            printf("%c",tolower(c[j]));
+        }
+        else if(count>count_1)
+        {
+            printf("%c",toupper(c[j]));
+        }
+        else if(count==count_1)
+        {
+            printf("%c",tolower(c[j]));
+        }
+    }
+
+}
