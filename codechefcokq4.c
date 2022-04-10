1b9043a8a3d02ecd176966e0450ccd7fefbccce8
HarshitAgarwal22000 <harshitiit2003@gmail.com>
2022-04-10 23:28:56 +0530

    commit

--- codechefcokq4.c
+++ codechefcokq4.c
@@ -3,18 +3,21 @@
 int main()
 {
     int i;
+    scanf("%d",&i);
     for(int j=1;j<=i;j++)
     {
+        int o=0;
         int y,x;
         int r,q;
         scanf("%d %d",&y,&x);
-        for(int e=y,e<=x;e++)
+        for(int e=y;e<=x;e++)
         {
-            for(int d=y;d<=x;d++)
+            for(int d=y+1;d<=x;d++)
             {
                 int z=fmin(e,d);
-                for(int u=1;u<=z;u++)
+                for(int u=z;u>=2;u--)
                 {
+                    
                     if(e%u==0 && d%u==0)
                     {
                         r=e;
@@ -33,7 +36,14 @@ int main()
                 break;
             }        
             }
-             printf("%d",r);
-             printf("%d",q);
+            if(r==q)
+            {
+               printf("%d \n",-1);
+            }
+            else
+            {
+             printf("%d ",r);
+             printf("%d \n",q);
+            }
     }
 }
